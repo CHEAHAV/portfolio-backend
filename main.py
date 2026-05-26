36bf0dee100b631785a7d06afbfc4f7f1c55f6e5
CHEAHAV <cheahav7171@gmail.com>
2026-05-26 22:58:27 +0700

    fix: add CORS middleware to website sub-app

--- main.py
+++ main.py
@@ -41,6 +41,14 @@ website = FastAPI(
     swagger_ui_parameters={"docExpansion": "none", "filter": True, "tagsSorter": "alpha",},
 )
 
+website.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
